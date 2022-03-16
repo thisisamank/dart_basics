9df2934cd0cab78e00955b5705dad00e31c4a928
Aman Kumar <thisisamank@gmail.com>
2022-03-16 19:09:26 +0530

    intial commit

---
+++ bin/dart_basics.dart
@@ -0,0 +1,25 @@
+// Hello World
+
+// Types : int, double, String, bool, dynamic
+
+// comments
+
+// var, final, const
+
+// if-else statements
+
+// switch statements
+
+// loops
+
+// functions
+
+// classes
+
+// inheritance
+
+// mixins
+
+// Extension methods
+
+// async-await
